21948a9a29e2836c4eb42fec778c77bf91b39100
bthoward <brianhoward@switch10.com>
2016-04-17 15:32:16 -0500

    Change output color.

--- RandomServerNames.ps1
+++ RandomServerNames.ps1
@@ -6,6 +6,6 @@
 for ($i=1;$i -le $Count; $i++) {
     $name = -join ((Get-Content C:\Script\names.txt | Get-Random).Trim(),"-",(Get-Random -Minimum 1001 -Maximum 1999))
 if (!(Test-Connection -computername "$c.dylan.net" -Count 1 -Quiet)) {
-    write-host $name.ToUpper() -foregroundcolor "magenta"
+    write-host $name.ToUpper() -foregroundcolor "green"
 }
 }
