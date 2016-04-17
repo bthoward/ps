08af7355c53a2c997d43193b469080a049c23892
bthoward <brianhoward@switch10.com>
2016-04-17 15:47:16 -0500

    Replaced get-content for names with array

--- RandomServerNames.ps1
+++ RandomServerNames.ps1
@@ -1,10 +1,12 @@
-﻿param(
+﻿
+param(
 [Parameter(Position=0,Mandatory=$true)][int]$Count
 )
 
-#$d = Read-Host "how many?"
+$PhoneticNames = "Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliet","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"
+
 for ($i=1;$i -le $Count; $i++) {
-    $name = -join ((Get-Content C:\Script\names.txt | Get-Random).Trim(),"-",(Get-Random -Minimum 1001 -Maximum 1999))
+    $name = -join (($PhoneticNames | Get-Random).Trim(),"-",(Get-Random -Minimum 1001 -Maximum 1999))
 if (!(Test-Connection -computername "$c.dylan.net" -Count 1 -Quiet)) {
     write-host $name.ToUpper() -foregroundcolor "green"
 }
